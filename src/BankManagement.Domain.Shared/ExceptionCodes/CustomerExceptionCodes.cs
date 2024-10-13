174b3765d5a3f9957715894ece781617449ddcfc
Emir Leylek <emrlylk4225@gmail.com>
2024-10-14 00:42:39 +0300

    #100-002 refactor:Created customer update,delete,get by id,get list endpoints and service methods.

--- src/BankManagement.Domain.Shared/ExceptionCodes/CustomerExceptionCodes.cs
+++ src/BankManagement.Domain.Shared/ExceptionCodes/CustomerExceptionCodes.cs
@@ -1,9 +1,37 @@
 namespace BankManagement.ExceptionCodes;
 
-public class CustomerExceptionCodes
+public static class CustomerExceptionCodes
 {
     private const string Prefix = "BankManagement";
     private const string CustomerExceptionCodesPrefix = $"{Prefix}.Customer";
     public const string NotFound = $"{CustomerExceptionCodesPrefix}:00001";
     public const string AlreadyExists = $"{CustomerExceptionCodesPrefix}:00002";
+    
+    public static class IdentityNumber
+    {
+        private const string PrefixId = $"{CustomerExceptionCodesPrefix}.IdentityNumber";
+        public const string CannotBeEmpty = $"{PrefixId}:00001";
+        public const string MaxLength = $"{PrefixId}:00002";
+    }
+    public static class Name
+    {
+        private const string PrefixId = $"{CustomerExceptionCodesPrefix}.Name";
+        public const string CannotBeEmpty = $"{PrefixId}:00001";
+    }
+    public static class Surname
+    {
+        private const string PrefixId = $"{CustomerExceptionCodesPrefix}.Surname";
+        public const string CannotBeEmpty = $"{PrefixId}:00001";
+    }
+    public static class Phone
+    {
+        private const string PrefixId = $"{CustomerExceptionCodesPrefix}.Phone";
+        public const string CannotBeEmpty = $"{PrefixId}:00001";
+        public const string MaxLength = $"{PrefixId}:00002";
+    }
+    public static class BirthDay
+    {
+        private const string PrefixId = $"{CustomerExceptionCodesPrefix}.BirthDay";
+        public const string CannotBeEmpty = $"{PrefixId}:00001";
+    }
 }
